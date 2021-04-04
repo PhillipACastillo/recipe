395df4fa5a72e6de587bbf93cfa753e351c19248
Phillip Castillo <PhillipACastillo@gmail.com>
2021-04-04 12:38:38 -0700

    Add ability to access database entities by description to all existing repositories. Test that these methods work using a print statement in the controller class

--- src/main/java/guru/springframework/recipe/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/recipe/repositories/CategoryRepository.java
@@ -3,5 +3,13 @@ package guru.springframework.recipe.repositories;
 import guru.springframework.recipe.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    // Extend the CrudRepository to find by attributes
+      // other than ID
+    // The optional class is required to prevent
+      // NullPointerExceptions
+    Optional<Category> findByDescription(String description);
 }
