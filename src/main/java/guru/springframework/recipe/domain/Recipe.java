de701be76790dc59ffa5acc19721245c1a624df3
Phillip Castillo <PhillipACastillo@gmail.com>
2021-04-04 11:24:27 -0700

    Add the category entity and set up its many-to-many JPA relationship with recipe. Additionally, add missing getters and setters to the recipe class

--- src/main/java/guru/springframework/recipe/domain/Recipe.java
+++ src/main/java/guru/springframework/recipe/domain/Recipe.java
@@ -31,9 +31,18 @@ public class Recipe {
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
+    // Do not forget to specify a join table
+      // Otherwise, Hibernate will create two relationship tables between
+      // the many to many entities
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+    joinColumns = @JoinColumn(name = "recipe_id"),
+    inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     // We specify the EnumType here to override the default (ORDINAL)
-    // When modified, the enumerated entity will not encounter any conflicts in the
-      // original database
+    // When modified, the enumerated entity will not encounter any
+      // conflicts in the original database
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
@@ -115,5 +124,29 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
